c2e5098d4b31eff2451fad3e0eb4a0c77d809dcc
Matej Havelka <M.Havelka@student.tudelft.nl>
2020-11-18 18:33:38 +0100

    Improve yellow localization

--- Localization.py
+++ Localization.py
@@ -20,24 +20,32 @@ Hints:
 
 
 def plate_detection(image):
+
     # Replace the below lines with your code.
     # https://stackoverflow.com/questions/57262974/tracking-yellow-color-object-with-opencv-python
     # https://docs.opencv.org/3.4/da/d97/tutorial_threshold_inRange.html
+    # https://stackoverflow.com/questions/22588146/tracking-white-color-using-python-opencv
+
     original = image.copy()
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    lower = np.array([15, 93, 0], dtype="uint8")
-    upper = np.array([45, 255, 255], dtype="uint8")
-    mask = cv2.inRange(image, lower, upper)
+    image_yellow = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    lower_yellow = np.array([15, 93, 100], dtype="uint8")
+    upper_yellow = np.array([45, 255, 255], dtype="uint8")
+    mask_yellow = cv2.inRange(image_yellow, lower_yellow, upper_yellow)
 
-    result = mask
+    image_white = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    lower_white = np.array([0, 0, 150])
+    upper_white = np.array([80, 80, 255])
+    mask_white = cv2.inRange(image_white, lower_white, upper_white)
 
-    #https://subscription.packtpub.com/book/data/9781789344912/7/ch07lvl1sec81/thresholding-color-images
-    # (b, g, r) = cv2.split(image)
-    # ret2, thresh2 = cv2.threshold(b, 0, 100, cv2.THRESH_BINARY)
-    # ret3, thresh3 = cv2.threshold(g, 100, 255, cv2.THRESH_BINARY)
-    # ret4, thresh4 = cv2.threshold(r, 100, 255, cv2.THRESH_BINARY)
-    # result = cv2.merge((thresh2, thresh3, thresh4))
+    mask = cv2.bitwise_or(mask_white, mask_yellow)
+    mask = mask_yellow
 
+    #https://docs.opencv.org/master/d9/d61/tutorial_py_morphological_ops.html
+    kernel = np.ones((9, 9), np.uint8)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+
+    result = cv2.bitwise_and(original, original, mask=mask)
 
     return result
 
