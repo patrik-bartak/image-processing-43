0a5aa40c8b2d612ee442ae6158790279c8496355
Patrik Barták <p.bartak@student.tudelft.nl>
2021-01-16 15:54:25 +0100

    Fix stoopid mistake

--- pattern_error_correction.py
+++ pattern_error_correction.py
@@ -5,10 +5,13 @@ def correct_errors(plate_strings):
 
 
 def simple_sequential_correction(plate_strings):
-    # MAX = 6  # minimum hamming distance between plates considered different
+    min_diff = 6  # minimum hamming distance between plates considered different
 
     for i in range(len(plate_strings) - 1):
-        if hamming_distance(plate_strings[i], plate_strings[i + 1]) > 5:
+        dist = hamming_distance(plate_strings[i], plate_strings[i + 1])
+        if dist is None:
+            continue
+        if dist >= min_diff:
             plate_strings[i] += " - NEW PLATE NEXT"
 
     return plate_strings
@@ -17,7 +20,7 @@ def simple_sequential_correction(plate_strings):
 def hamming_distance(str1, str2):
     dist = 0
     if len(str1) != len(str2):
-        return "dist not equal"
+        return None
     for i in range(len(str1)):
         if str1[i] != str2[i]:
             dist += 1
